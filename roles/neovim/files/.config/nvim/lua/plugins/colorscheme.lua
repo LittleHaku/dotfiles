17239272f9b4cfc70bda0259d3584a86545d5ce4
LittleHaku <ivandelhorno@gmail.com>
2025-06-09 15:28:28 +0200

    dracula colorscheme

---
+++ roles/neovim/files/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "Mofiqul/dracula.nvim" },
+  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = { contrast = "hard" } },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+}
